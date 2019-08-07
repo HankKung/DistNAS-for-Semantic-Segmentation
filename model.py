35033bc6fcda49b4529b00b85d9056d69126045e
Hank Kung <hank910140@gmail.com>
2019-08-08 04:27:02 +0800

    ASPP

--- model.py
+++ model.py
@@ -7,15 +7,21 @@ from utils import drop_path
 
 class Cell(nn.Module):
 
-  def __init__(self, genotype, C_prev_prev, C_prev, C, reduction, reduction_prev, rate):
+  def __init__(self, genotype, C_prev_prev, C_prev, C, rate):
     super(Cell, self).__init__()
     print(C_prev_prev, C_prev, C)
 
     if rate==2:
-      self.preprocess0 = FactorizedReduce(C_prev_prev, C)
+      self.preprocess1 = FactorizedReduce(C_prev, C)
+    elif rate == 0 :
+      self.preprocess1 = FactorizedIncrease (C_prev, C)
+    else:
+      self.preprocess1 = ReLUConvBN(C_prev, C, 1, 1, 0)
+
+    if C_prev_prev!=C:
+      self.preprocess0 = Resize_bilinear(C_prev_prev, C, C_prev_prev/C)
     else:
       self.preprocess0 = ReLUConvBN(C_prev_prev, C, 1, 1, 0)
-    self.preprocess1 = ReLUConvBN(C_prev, C, 1, 1, 0)
     
     op_names, indices = zip(*genotype.normal)
     concat = genotype.normal_concat
@@ -29,8 +35,7 @@ class Cell(nn.Module):
 
     self._ops = nn.ModuleList()
     for name, index in zip(op_names, indices):
-      stride = 2 if reduction and index < 2 else 1
-      op = OPS[name](C, stride, True)
+      op = OPS[name](C, 1, True)
       self._ops += [op]
     self._indices = indices
 
@@ -55,22 +60,74 @@ class Cell(nn.Module):
       states += [s]
     return torch.cat([states[i] for i in self._concat], dim=1)
 
+class ASPP(nn.Module):
+
+    def __init__(self, C, depth, num_classes, conv=nn.Conv2d, norm=nn.BatchNorm2d, momentum=0.0003, mult=1):
+        super(ASPP, self).__init__()
+        self._C = C
+        self._depth = depth
+        self._num_classes = num_classes
+
+        self.global_pooling = nn.AdaptiveAvgPool2d(1)
+        self.relu = nn.ReLU(inplace=True)
+        self.aspp1 = conv(C, depth, kernel_size=1, stride=1, bias=False)
+        self.aspp2 = conv(C, depth, kernel_size=3, stride=1,
+                               dilation=int(6*mult), padding=int(6*mult),
+                               bias=False)
+        self.aspp3 = conv(C, depth, kernel_size=3, stride=1,
+                               dilation=int(12*mult), padding=int(12*mult),
+                               bias=False)
+        self.aspp4 = conv(C, depth, kernel_size=3, stride=1,
+                               dilation=int(18*mult), padding=int(18*mult),
+                               bias=False)
+        self.aspp5 = conv(C, depth, kernel_size=1, stride=1, bias=False)
+        self.aspp1_bn = norm(depth, momentum)
+        self.aspp2_bn = norm(depth, momentum)
+        self.aspp3_bn = norm(depth, momentum)
+        self.aspp4_bn = norm(depth, momentum)
+        self.aspp5_bn = norm(depth, momentum)
+        self.conv2 = conv(depth * 5, depth, kernel_size=1, stride=1,
+                               bias=False)
+        self.bn2 = norm(depth, momentum)
+        self.conv3 = nn.Conv2d(depth, num_classes, kernel_size=1, stride=1)
+
+    def forward(self, x):
+        x1 = self.aspp1(x)
+        x1 = self.aspp1_bn(x1)
+        x1 = self.relu(x1)
+        x2 = self.aspp2(x)
+        x2 = self.aspp2_bn(x2)
+        x2 = self.relu(x2)
+        x3 = self.aspp3(x)
+        x3 = self.aspp3_bn(x3)
+        x3 = self.relu(x3)
+        x4 = self.aspp4(x)
+        x4 = self.aspp4_bn(x4)
+        x4 = self.relu(x4)
+        x5 = self.global_pooling(x)
+        x5 = self.aspp5(x5)
+        x5 = self.aspp5_bn(x5)
+        x5 = self.relu(x5)
+        x5 = nn.Upsample((x.shape[2], x.shape[3]), mode='bilinear',
+                         align_corners=True)(x5)
+        x = torch.cat((x1, x2, x3, x4, x5), 1)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+        x = self.conv3(x)
+
+        return x
+
 
 class Network_Device(nn.Module):
 
-  def __init__(self, C, num_classes, layers, genotype):
+  def __init__(self, num_classes, layers, genotype, backbone):
     super(NetworkCIFAR, self).__init__()
-    self._layers = layers
-    #self._auxiliary = auxiliary
-
-    stem_multiplier = 3
-    C_curr = 64
-    self.stem = nn.Sequential(
-      nn.Conv2d(3, C_curr, 3, stride=2, padding=1),
-      nn.BatchNorm2d(C_curr),
+    self.stem0 = nn.Sequential(
+      nn.Conv2d(3, 64, 3, stride=2, padding=1),
+      nn.BatchNorm2d(64),
       nn.ReLU ()
     )
-
     self.stem1 = nn.Sequential(
       nn.Conv2d(64, 64, 3, padding=1),
       nn.BatchNorm2d(64),
@@ -82,77 +139,78 @@ class Network_Device(nn.Module):
       nn.ReLU ()
     )
 
-    C_prev_prev, C_prev, C_curr = 64, 128, C
+    C_prev_prev, C_prev = 64, 128, 
     self.cells = nn.ModuleList()
-    reduction_prev = False
+    filter_base = 100
+    C_curr=filter_base
+    for cell_num in range(len(backbone)):
+      rate=1
+      if cell_num==0:
+        C_curr = filter_base*backbone[0]/4
+      else: 
+        if backbone[cell_num]*2==backbone[cell_num-1]:
+          C_curr=C_curr/2
+          rate = 0
+        elif backbone[cell_num]//2==backbone[cell_num-1]:
+          C_curr=C_curr*2
+          rate = 2
+      cell = Cell(genotype, C_prev_prev, C_prev, C_curr, rate)
 
-    filter_scale = 1.0
-    for i in range(layers):
-      if i in [layers//3, 2*layers//3]:
-        C_curr *= 2
-        reduction = True
-      else:
-        reduction = False
-      cell = Cell(genotype, C_prev_prev, C_prev, C_curr, reduction, reduction_prev)
-      reduction_prev = reduction
       self.cells += [cell]
-      C_prev_prev, C_prev = C_prev, cell.multiplier*C_curr
-      #if i == 2*layers//3:
-      #  C_to_auxiliary = C_prev
+      C_prev_prev, C_prev = C_prev, C_curr
 
-    #if auxiliary:
-      #self.auxiliary_head = AuxiliaryHeadCIFAR(C_to_auxiliary, num_classes)
     self.global_pooling = nn.AdaptiveAvgPool2d(1)
     self.classifier = nn.Linear(C_prev, num_classes)
 
   def forward(self, input):
-    #logits_aux = None
-    s0 = s1 = self.stem(input)
+    y = self.stem0(input)
+    s0 = self.stem1(y)
+    s1 = self.stem2(s0)
+
     for i, cell in enumerate(self.cells):
       s0, s1 = s1, cell(s0, s1, self.drop_path_prob)
-      if i == 2*self._layers//3:
-        #if self._auxiliary and self.training:
-        #  logits_aux = self.auxiliary_head(s1)
+
     out = self.global_pooling(s1)
     logits = self.classifier(out.view(out.size(0),-1))
     return logits
 
 class Dist_Network(nn.Module):
 
-  def __init__(self, C, num_classes, layers, genotype):
+  def __init__(self, C, num_classes, layers, genotype, backbone):
     super(NetworkCIFAR, self).__init__()
-    self._layers = layers
-    #self._auxiliary = auxiliary
     
-    C_prev_prev, C_prev, C_curr = C_curr, C_curr, C
+    C_prev_prev, C_prev = 64, 128, 
     self.cells = nn.ModuleList()
-    reduction_prev = False
-    for i in range(layers):
-      if i in [layers//3, 2*layers//3]:
-        C_curr *= 2
-        reduction = True
-      else:
-        reduction = False
-      cell = Cell(genotype, C_prev_prev, C_prev, C_curr, reduction, reduction_prev)
-      reduction_prev = reduction
-      self.cells += [cell]
-      C_prev_prev, C_prev = C_prev, cell.multiplier*C_curr
-      #if i == 2*layers//3:
-      #  C_to_auxiliary = C_prev
+    filter_base = 100
+    C_curr=filter_base
+    for cell_num in range(len(backbone)):
+      rate=1
+      if cell_num==0:
+        C_curr = filter_base*backbone[0]/4
+      else: 
+        if backbone[cell_num]*2==backbone[cell_num-1]:
+          C_curr=C_curr/2
+          rate = 0
+        elif backbone[cell_num]//2==backbone[cell_num-1]:
+          C_curr=C_curr*2
+          rate = 2
+      cell = Cell(genotype, C_prev_prev, C_prev, C_curr, rate)
 
-    #if auxiliary:
-      #self.auxiliary_head = AuxiliaryHeadCIFAR(C_to_auxiliary, num_classes)
+      self.cells += [cell]
+      C_prev_prev, C_prev = C_prev, C_curr
+
     self.global_pooling = nn.AdaptiveAvgPool2d(1)
     self.classifier = nn.Linear(C_prev, num_classes)
 
+
   def forward(self, input):
-    #logits_aux = None
-    s0 = s1 = self.stem(input)
+    y = self.stem0(input)
+    s0 = self.stem1(y)
+    s1 = self.stem2(s0)
+
     for i, cell in enumerate(self.cells):
       s0, s1 = s1, cell(s0, s1, self.drop_path_prob)
-      if i == 2*self._layers//3:
-        #if self._auxiliary and self.training:
-        #  logits_aux = self.auxiliary_head(s1)
+
     out = self.global_pooling(s1)
     logits = self.classifier(out.view(out.size(0),-1))
     return logits
